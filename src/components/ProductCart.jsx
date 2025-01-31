df23251e290baca101932f7aa89d60f6062f3a34
Md. Delwar Hossain <delwarbscse@gmail.com>
2025-02-01 04:50:38 +0600

    Reset image size in shop

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -9,8 +9,8 @@ const ProductCart = ({singleProduct}) => {
       <span className='flex justify-end'>
         <RiShoppingCartLine className='text-font02' size={28}/>
       </span>
-      <div className='flex justify-center items-center my-10 md:my-16 w-full h-[220px] overflow-hidden'>
-        <img src={singleProduct?.productImg} alt="product image" className='w-full h-auto scale-90  group-hover:scale-100 transition-all duration-300' />
+      <div className='flex justify-center items-center my-10 md:my-16 w-full overflow-hidden'>
+        <img src={singleProduct?.productImg} alt="product image" className='w-[200px] h-[200px] scale-90  group-hover:scale-100 transition-all duration-300' />
       </div>
       <p className='text-xl py-4'>The Ball</p>
       <p className='text-3xl pb-4'>$ 34.00 CDN</p>
